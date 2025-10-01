51129f1a39435589590fff4092669bc4ddafb72a
Idra Hsieh <idra.hsieh@gmail.com>
2025-10-02 00:47:35 +0800

    feat(about): add tab navigation

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -86,7 +86,48 @@ import { motion } from "framer-motion";
 
 const About = () => {
     return (
-        <div>about page</div>
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{
+                opacity: 1,
+                transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+            }}
+            className='min-h-[80vh] flex items-center justify-center py-12 xl: py-0'
+        >
+            <div className='container mx-auto'>
+                <Tabs
+                    defaultValue='experience'
+                    className='flex flex-col xl:flex-row gap-[60px]'
+                >
+                    <TabsList className='flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6'>
+                        <TabsTrigger value="experience">Experience</TabsTrigger>
+                        <TabsTrigger value="education">Education</TabsTrigger>
+                        <TabsTrigger value="skills">Skills</TabsTrigger>
+                        <TabsTrigger value="beyond">Beyond</TabsTrigger>
+                    </TabsList>
+
+                    {/* content */}
+                    <div className='min-h-[70vh] w-full'>
+                        {/* experience */}
+                        <TabsContent value="experience" className='w-full'>
+                            experience
+                        </TabsContent>
+                        {/* education */}
+                        <TabsContent value="education" className='w-full'>
+                            education
+                        </TabsContent>
+                        {/* skills */}
+                        <TabsContent value="skills" className='w-full'>
+                            skills
+                        </TabsContent>
+                        {/* beyond */}
+                        <TabsContent value="beyond" className='w-full'>
+                            beyond
+                        </TabsContent>
+                    </div>
+                </Tabs>
+            </div>
+        </motion.div>
     )
 }
 
