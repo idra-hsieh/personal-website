b8a6b6c3ee3138067111bd53785ac67da3abd336
Idra Hsieh <idra.hsieh@gmail.com>
2025-10-03 02:14:56 +0800

    feat(home): add Substack to socials and tooltip hover labels for all icons

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,11 +1,20 @@
 import Link from "next/link";
 
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { SiSubstack } from "react-icons/si";
+
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 const socials = [
-    { icon: <FaGithub />, path: "https://github.com/idra-hsieh" },
-    { icon: <FaLinkedin />, path: "https://www.linkedin.com/in/idra-hsieh/" },
-]
+    { icon: <FaGithub />, path: "https://github.com/idra-hsieh", name: "GitHub" },
+    { icon: <FaLinkedin />, path: "https://www.linkedin.com/in/idra-hsieh/", name: "LinkedIn" },
+    { icon: <SiSubstack />, path: "https://thebuilderslibrary.substack.com/", name: "Substack" },
+];
 
 // Define props type
 
@@ -14,20 +23,28 @@ type SocialsProps = {
     iconStyles?: string;
 };
 
-const Socials = ({ containerStyles, iconStyles }) => {
+const Socials = ({ containerStyles, iconStyles }: SocialsProps) => {
     return (
         <div className={containerStyles}>
             {socials.map((item, index) => {
                 return (
-                    <Link
-                        key={index}
-                        href={item.path}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className={iconStyles}
-                    >
-                        {item.icon}
-                    </Link>
+                    <TooltipProvider key={index} delayDuration={100}>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link
+                                    href={item.path}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className={iconStyles}
+                                >
+                                    {item.icon}
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                <p>{item.name}</p>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
                 );
             })}
         </div>
