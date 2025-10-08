37312e644e958d0483bb10e35c401dde750913b1
Idra Hsieh <idra.hsieh@gmail.com>
2025-10-08 17:54:09 +0800

    feat(contact): add textarea

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -68,6 +68,11 @@ const Contact = () => {
                                     </SelectGroup>
                                 </SelectContent>
                             </Select>
+                            {/* textarea */}
+                            <Textarea
+                                className="h-[200px]"
+                                placeholder="Type your message here."
+                            />
                         </form>
                     </div>
                     {/* info */}
--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -7,7 +7,10 @@ function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
     <textarea
       data-slot="textarea"
       className={cn(
-        "border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "flex min-h-[80px] w-full rounded-md boarder border-white/10",
+        "bg-primary px-4 py-5 text-base placeholder:text-foreground/40",
+        "focus-visible:outline-none focus-visible:ring-1 focus-visible:ring=accent",
+        "focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       {...props}
