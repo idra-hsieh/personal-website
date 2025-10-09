91974a558aabcd5afecb7228e847fe9481086911
Idra Hsieh <idra.hsieh@gmail.com>
2025-10-09 22:56:56 +0800

    chore(setup): re-install sanity

--- sanity/client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "next-sanity";
-
-export const client = createClient({
-    projectId: "m0pg3g08",
-    dataset: "blog",
-    apiVersion: "2024-01-01",
-    useCdn: false,
-});
