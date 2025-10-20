64c92fd2e31bede5325cbe198e8b51bf672f1ab1
Idra Hsieh <idra.hsieh@gmail.com>
2025-10-20 22:27:22 +0800

    chore(eslint): customize ESLint config and disable unnecessary strict rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,12 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/quotes": "off",
+      quotes: ["off"],
+      "no-useless-escape": "off"
+    },
   },
 ];
 
