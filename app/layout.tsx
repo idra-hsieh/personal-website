419065bbc415e165841093056fc2c337f07c1169
Idra Hsieh <idra.hsieh@gmail.com>
2025-10-21 00:22:00 +0800

    fix: correct Header import case for Linux build

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Fraunces, Nunito_Sans } from "next/font/google";
 import "./globals.css";
 
 // components
-import Header from "@/components/header";
+import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/WaveTransition";
 
