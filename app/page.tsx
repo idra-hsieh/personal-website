f0859b9a522af9fb69dcfc6311fcbb2b65429455
Idra Hsieh <idra.hsieh@gmail.com>
2025-10-20 22:44:30 +0800

    chore(main): add placeholder CV file

--- app/page.tsx
+++ app/page.tsx
@@ -25,20 +25,22 @@ const Home = () => {
 
             {/* button and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button
-                variant="outline"
-                size="lg"
-                className="uppercase flex itens-center gap-2"
-              >
-                <span>Download my CV</span>
-                <FiDownload className="text-xl" />
-              </Button>
+              <a href="/assets/main/CV_Idra_Hsieh.pdf" target="_blank" rel="noopener noreferrer">
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className="uppercase flex itens-center gap-2"
+                >
+                  <span>Download my CV</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+              </a>
               <div className="mb-8 xl:mb-0">
                 <Socials
                   containerStyles="flex gap-6"
                   iconStyles="w-9 h-9 border border-accent rounded-full flex
-                  justify-center items-center text-accent text-base hover:bg-accent
-                  hover:text-primary hover:transition-all duration-500"
+                    justify-center items-center text-accent text-base hover:bg-accent
+                    hover:text-primary hover:transition-all duration-500"
                 />
               </div>
             </div>
